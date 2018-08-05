1026ddf7370736bcdbaaec37dd684d2130106b32
coxipozina <coxipozina@gmail.com>
2018-08-04 17:51:24 -0700

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Straight from your lips to your hips burguer', false);
